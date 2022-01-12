ac2911fcbdc1c51de1ecb52b371065ebce7aecb5
Rich FitzJohn <rich.fitzjohn@gmail.com>
2022-01-12 10:22:42 +0000

    version 1.1.3

--- tests/testthat/test-connection-redux.R
+++ tests/testthat/test-connection-redux.R
@@ -4,14 +4,38 @@ test_that("impossible connection", {
   skip_if_no_redis()
   expect_error(redis_connection(redis_config(port = 99999)),
                "Failed to create context")
+  ## this does not trigger on windows with hiredis v1, for unknown
+  ## reasons
+  skip_on_os("windows")
   expect_error(redis_connect_unix(tempfile()),
                "Failed to create context")
 })
 
+test_that("connection timeout over tcp returns sensible error", {
+  skip_if_no_redis()
+  cfg <- redis_config(host = "example.com", timeout = 1000)
+  ## The error here *might* say
+  ##
+  ##     Failed to create context: Connection timed out
+  ##
+  ## or similar, but this seems very dependent on (presumably) hiredis
+  ## client versions and platforms.  On windows we only see "unknown
+  ## error" and on mac the string differs slightly.  We don't control
+  ## the exact message so should just report on the part we do control.
+  t <- system.time(
+    expect_error(
+      redis_connection(cfg),
+      "Failed to create context"))
+
+  ## These are likely to prove a bit flakey due to the usual issues
+  ## with timing.
+  expect_lt(t[["elapsed"]], 2)
+  expect_gte(t[["elapsed"]], 1)
+})
+
 test_that("auth", {
   skip_if_no_redis()
-  expect_error(redis_connection(redis_config(password = "foo")),
-               "no password is set")
+  expect_error(redis_connection(redis_config(password = "foo")))
 })
 
 test_that("select db", {
--- tests/testthat/test-redis-socket.R
+++ tests/testthat/test-redis-socket.R
@@ -3,18 +3,24 @@ context("socket connection")
 test_that("socket connection", {
   skip_if_no_redis()
   skip_on_cran()
+
   redis_server <- Sys.which("redis-server")
   if (redis_server == "") {
     skip("didn't find redis server")
   }
-  logfile <- tempfile("redis_")
-  socket <- tempfile("socket_")
+  tmp <- tempfile()
+  dir.create(tmp, FALSE, TRUE)
+  tmp <- normalizePath(tmp)
+
+  logfile <- file.path(tmp, "log")
+  socket <- file.path(tmp, "socket")
+
   system2(redis_server, c("--port", 0, "--unixsocket", socket),
           wait = FALSE, stdout = logfile, stderr = logfile)
   Sys.sleep(.5)
 
   if (!file.exists(socket)) {
-    ## This does leave a redis server running on a socket!
+    ## This might leave a redis server running on a socket!
     skip("Didn't start socket server")
   }
   config <- redis_config()
@@ -25,6 +31,9 @@ test_that("socket connection", {
   expect_equal(redis_command(ptr_tcp,  list("PING")), cmp)
 
   tmp <- hiredis(redis_config(path = socket))
+  expect_equal(tmp$PING(), cmp)
+
+  tmp <- hiredis(redis_config(path = socket, timeout = 1000))
   expect_equal(tmp$PING(), cmp)
 
   expect_error(redis_command(ptr_sock, "SHUTDOWN"),
